f6f0bd2540928645fdcf13f95e73658ea3d4d221
Gan Eng Chin <ecgan@users.noreply.github.com>
2025-05-25 00:01:35 +0800

    More elegant solution by using `pairsMap` without using `start` and `ends`.

--- 2-live-coding-interview/randomWord.ts
+++ 2-live-coding-interview/randomWord.ts
@@ -1,19 +1,17 @@
 function getRandomIntInclusive(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
 }
 
 const randomWord = (words: string[]) => {
     const pairsMap = new Map<string, Set<string>>()
 
-    const start: string[] = []
-    const ends = new Set()
-
     for (let i = 0; i < words.length; i++) {
         const word = words[i];
         
-        for (let j = 0; j < word.length - 1; j++) {
+        for (let j = 0; j < word.length; j++) {
+            // iterate over the letters in the word.
             const letter = word[j];
             
             if (!pairsMap.has(letter)) { 
@@ -21,32 +19,23 @@ const randomWord = (words: string[]) => {
             }
 
             const pairs = pairsMap.get(letter)
-            const nextLetter = word[j+1]
+            const nextLetter = word[j+1] || ''
             pairs?.add(nextLetter)
-
-            if (j === word.length - 2) {
-                ends.add(word[word.length - 1])
-            }
-
-            if (j === 0){
-                start.push(letter)
-            }
         }
     }
 
-    const num = getRandomIntInclusive(0, start.length - 1 )
+    const num = getRandomIntInclusive(0, words.length - 1 )
 
     const result: string[] = []
-    result.push(start[num])
-    console.log('result:',result)
-    console.log('ends:',ends)
+    result.push(words[num][0])
 
-    while(!ends.has(result[result.length-1])) {
+    while(true) {
         const lastLetter = result[result.length-1]
-        console.log('lastLetter:',lastLetter)
+        if (lastLetter === '') {
+            break;
+        }
 
-        const letters = pairsMap.get(lastLetter)
-        console.log('letters:',letters)
+        const letters = pairsMap.get(lastLetter) || new Set()
 
         const num = getRandomIntInclusive(0, letters?.size - 1)
         const nextLetter = Array.from(letters?.values() || [])[num]
